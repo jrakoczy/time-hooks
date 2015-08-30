03742392291e4620eb1a4e8905f72b868887e3c6
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2015-08-30 19:13:23 +0200

    Fix comments and tidy up

--- toggl.sh
+++ toggl.sh
@@ -1,54 +1,61 @@
 #!/bin/sh
 . ./auxiliary.sh
 
-# Regexes
+# Define regexes
 hours_regex="[0-9]+"
 minutes_regex="[0-6][0-9]|[1-9]" # Both double- and single-digit values are valid
 time_regex="^Time spent:([[:space:]]*$hours_regex[[:space:]]*h)?[[:space:]]*$minutes_regex[[:space:]]*min[[:space:]]*$"
 
 committers_regex="^Committers:([[:space:]]*([[:alnum:]]*)[[:space:]]*)+$"
 
-# Get relevant lines
-#subject_line="$(git log -1 HEAD --pretty=format:%s)"
-#message_body="$(git log -1 HEAD --pretty=format:%b)"
-message_body="Time spent: 1h 20 min
-Committers: jrakoczy"
 
-subject_line="Final test"
+# Retrieve relevant lines from a commit message
+message_body="$(git log -1 HEAD --pretty=format:%b)"
+
+subject_line="$(git log -1 HEAD --pretty=format:%s)"
 time_line="$(printf "%s" "$message_body" | egrep "$time_regex")"
 committers_line="$(printf "%s" "$message_body" | egrep "$committers_regex")"
 
-# Convert time to particular units
-if [ "$time_line" ] && [[ "$time_line" =~ .*h.*  ]] 
-then   
-    hours="$(printf "%s" "$time_line" | sed -E "s/[^0-9]*($hours_regex)[[:space:]]*h.*/\1/")"
-fi
+
+# Convert retrieved data to particular time units
+[ ! "$time_line" ] && { printf "[ERROR] No line contains properly formatted time data.\n"; exit 1; }
+
+[[ "$time_line" =~ .*h.*  ]] &&\
+hours="$(printf "%s" "$time_line" | sed -E "s/[^0-9]*($hours_regex)[[:space:]]*h.*/\1/")"
 
 minutes="$(printf "%s" "$time_line" | sed -E "s/.*h[^0-9]*($minutes_regex)[[:space:]]*min.*/\1/")"
 seconds="$(( hours * 3600 + minutes * 60 ))"
 
-# Get Toggl API keys
-committers="$(printf "%s" "$committers_line" | sed "s/Committers:[[:space:]]*//")"
-handles_array=(${committers})
-
+# Get project id
 project_id="$(git config --get toggl.pid 2>/dev/null)"  
+
 if [ ! "$project_id" ]
 then 
     printf "\n[ERROR] A project id is not defined."
     yesno_message="$(printf "\nWould you like to add a project id? (Y/n): ")"
-    yesno "$yesno_message" || { printf "Couldn't add a time entry."; exit 1; }
-    printf "\nProject id [$handle]: " 
+    yesno "$yesno_message" || { printf "\n[ERROR] Couldn't add a time entry.\n"; exit 1; }
+    printf "\nProject id: " 
     read project_id
 fi
 
+
+# Get committers from a commit message
+committers="$(printf "%s" "$committers_line" | sed "s/Committers:[[:space:]]*//")"
+[ ! "$committers" ] && { printf "\n[ERROR] No line contains properly formatted committers data.\n"; exit 1; }
+handles_array=(${committers})
+
+
+# Get Toggl API keys and send request(s)
 send_request() {
+    # @params:
+    #        $1 -- an API key
+
     local header="Content-Type: application/json"
     local request_url="https://www.toggl.com/api/v8/time_entries"
     local auth="$1:api_token"
     
     # Data
-    local start_time="$(date +"%FT%T+02:00" --date "-$seconds sec")"
-    echo "$start_time"
+    local start_time="$(date +"%FT%T+02:00" --date "-$seconds sec")" # +02:00 to adjust to a time zone
     local data="
                 {
                     \"time_entry\":
@@ -63,15 +70,14 @@ send_request() {
     
     if curl -v -u "$auth" -H "$header" -d "$data" -X POST "$request_url" >/dev/null 2>&1
     then
-        printf "Time entry added to the project." 
+        printf "\nTime entry added to the project." 
     else
-        printf "[ERROR] Couldn't access Toggl API. Time entry hasn't been added."
+        printf "\n[ERROR] Couldn't access Toggl API. Time entry hasn't been added.\n"
         exit 1
     fi             
 }
 
-
-for handle in "${handles_array[@]}"
+for handle in "${handles_array[@]}" # One request try for each handle 
 do
     api_key="$(git config --get toggl.key.$handle 2>/dev/null)"  
     if [ ! "$api_key" ]
